d58703743d132cf01c9700f7b5ae84cef9460a83
Le217spr1s <99697492+Le217spr1s@users.noreply.github.com>
2022-02-21 22:30:00 -0600

    Add files via upload

---
+++ Exercise 2main.js
@@ -0,0 +1,7 @@
+//Create the HTML with a button
+//Create a function that takes in a _user_ object
+//Print each item in the user object out in a string to the console
+function loanoffer(amount = 180000) {
+    console.log("Total Loan: ", loan);
+    return loan;
+};
